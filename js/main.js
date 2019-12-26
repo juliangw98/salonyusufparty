049654cc623cde0fe93c8d395ba7c9862046030f
julian <julian.gio.wieckardt@gmail.com>
2019-12-26 17:23:43 +0100

    go

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 
 $(window).on('load', function () {
 $('.fadeInNow').addClass("fadeInAnimation");
